a5bc4985ce5dfb9c8b3e436c1e858e80f186316e
tuxKOH <yumicheng14@gmail.com>
2026-09-02 06:23:58 +0800

    Release 0.3.5 dynamic table fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         applicationId = "io.github.tuxkoh.bcsfe"
         minSdk = 26
         targetSdk = 34
-        versionCode = 27
-        versionName = "0.3.4"
+        versionCode = 28
+        versionName = "0.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("boolean", "ADS_ENABLED", adsEnabled.toString())
