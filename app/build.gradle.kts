a363830c022bfcfe61944ebb50e2f1ec07e0aff0
tuxKOH <yumicheng14@gmail.com>
2026-09-05 21:32:27 +0800

    Move rank notification action to User Rank rewards

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         applicationId = "io.github.tuxkoh.bcsfe"
         minSdk = 26
         targetSdk = 34
-        versionCode = 30
-        versionName = "1.1.0"
+        versionCode = 31
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("boolean", "ADS_ENABLED", adsEnabled.toString())
